4486edfbe4620f92d20f79ef1e470126eea2e9d0
DjConsti <c.brincoveanu@yahoo.com>
2018-02-04 13:50:15 +0100

    add main method calls

--- src/cb/Main.java
+++ src/cb/Main.java
@@ -1,8 +1,14 @@
 package cb;
 
+import cb.ml.DecisionTree;
+import cb.ml.LinearRegression;
+import cb.ml.NeuralNetwork;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        DecisionTree.main(args);
+        LinearRegression.main(args);
+        NeuralNetwork.main(args);
     }
 }
